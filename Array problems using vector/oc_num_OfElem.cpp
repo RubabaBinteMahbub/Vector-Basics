290e3b6853b87fb3ecc3ca3836d3d07808e2ddaf
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2025-12-23 21:23:47 +0600

    Add program to count occurrences in vector

---
+++ Array problems using vector/oc_num_OfElem.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+int n,num,count=0;
+cout<<"Enter the size : ";
+cin>>n;
+cout<<"Enter the number : ";
+cin>>num;
+
+vector<int>a(n);
+cout<<"Enter the elements : ";
+for(int i=0;i<n;i++){
+   cin>>a[i];
+}
+  
+for(int i=0;i<a.size();i++){
+		if(a[i]==num){
+		count++;
+		}
+}
+cout<<num<<" occurred "<<count<<" times in the given vector."<<endl;
+return 0;
+}
