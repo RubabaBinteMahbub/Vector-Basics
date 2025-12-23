7807ca92f856d54c5a7206e42ebf0101bc5f5102
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2025-12-23 21:14:30 +0600

    Implement vector input and output in C++
    Fix input handling and output formatting in vector_input4.cpp.

---
+++ Array problems using vector/vector_input4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+vector<int>a(n);
+
+int i=0;
+while(i<size){
+cin>>a[i]; 
+i++
+}
+
+for(int i=0;i<a.size();i++){
+cout<<a[i]<<" ";
+}
+
+return 0;
+}
